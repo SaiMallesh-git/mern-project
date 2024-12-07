0cd637288984025e33962814a9d41cc5faddd4d1
SaiMallesh-git <2300030096@kluniversity.in>
2024-12-08 00:39:35 +0530

    Major Update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const userControllers = require("../../controllers/userControllers");
+const userControllers = require("../controllers/userControllers");
 const { check } = require("express-validator");
-const { verifyToken } = require("../../middleware/auth");
+const { verifyToken } = require("../middleware/auth");
 
 router.post("/register", userControllers.register);
 router.post("/login", userControllers.login);
